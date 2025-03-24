55b7e83c528eb4beff385b52dce28117834abe5b
Vũ Minh Thành <thanhthbm@gmail.com>
2025-03-24 16:01:02 +0700

    using Cloudinary as cdn

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const storageMulter = require('../../helpers/storageMulter.js');
-const upload = multer( { storage: storageMulter()});
 const controller = require('../../controllers/admin/product.controller.js');
 const validate = require('../../validates/admin/product.validate.js');
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware.js');
 
 router.get('/', controller.index);
 router.patch('/change-status/:status/:id', controller.changeStatus);
 router.patch('/change-multi', controller.changeMulti);
 router.delete('/delete/:id', controller.deleteItem);
 
+
+const upload = multer();
+
 //no need to pass req and res args to router
 router.get('/create', controller.create);
-router.post('/create', upload.single('thumbnail'), validate.createPost, controller.createPost);
+router.post(
+    '/create',
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.createPost
+);
 
 router.get('/edit/:id', controller.edit);
-router.patch('/edit/:id', upload.single('thumbnail'), validate.createPost, controller.editPatch);
+router.patch(
+    '/edit/:id',
+    upload.single('thumbnail'),
+    validate.createPost,
+    controller.editPatch
+);
 router.get('/detail/:id', controller.detail);
 
 module.exports = router;
